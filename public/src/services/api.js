6d3635138d2be9b370b15b5442543a0dd8fe5293
Gowthaman Ilango <gilango@eab.com>
2018-12-25 12:25:12 -0500

    added ec2 ip address

--- public/src/services/api.js
+++ public/src/services/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default ()=>{
     return axios.create({
         // baseURL: `https://9vcou25xv3.execute-api.us-east-1.amazonaws.com/dev/`
-        baseURL: `http://localhost:8081/`
+        // baseURL: `http://localhost:8081/`
+        baseURL: `http://52.207.132.249:8081` //dev ec2
     })
 }
